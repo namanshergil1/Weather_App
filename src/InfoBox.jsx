ffec6bb272b4de0ffaf4a9aab4a7310d6a22d0d5
namanshergil1 <143255076+namanshergil1@users.noreply.github.com>
2024-12-25 14:47:46 +0530

    Add files via upload

---
+++ src/InfoBox.jsx
@@ -0,0 +1,50 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import "./InfoBox.css";
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+
+
+export default function InfoBox({info}){
+    
+    const INIT_URL = "https://images.unsplash.com/photo-1722858343990-1604f540c15d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+    let HOT_URL="https://images.unsplash.com/photo-1566773333308-d2989f01cbc3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGhvdCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+    let COLD_URL="https://plus.unsplash.com/premium_photo-1670604649107-a0171e5f1bd0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+    let RAIN_URL="https://images.unsplash.com/photo-1475116127127-e3ce09ee84e1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHJhaW4lMjB3ZWF0aGVyfGVufDB8fDB8fHww";
+
+   
+
+    return(
+        <div className="InfoBox">
+        
+        <div className="cardContainer">
+            <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={info.humidity>80 ? RAIN_URL : info.temp>15 ? HOT_URL : COLD_URL}
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {info.city} {info.humidity>80 ? <ThunderstormIcon/>: info.temp>15 ? <WbSunnyIcon/> : <AcUnitIcon/>}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" component={"span"}>
+          <p>Temperature={info.temp}&deg;C</p>
+          <p>Humidity={info.humidity}</p>
+          <p>Min Temp={info.tempMin}&deg;C</p>
+          <p>Max Temp={info.tempMax}&deg;C</p>
+          <p>The Weather can be described as <i>{info.weather}</i> and Feels Like={info.feelsLike}&deg;C</p>
+
+
+        </Typography>
+      </CardContent>
+     
+    </Card>
+    </div>
+        </div>
+    );
+}
